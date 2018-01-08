6bbd48c973456a268686d34d55bc2914822b1b2a
Soumyajit Dutta <sanudatta11@gmail.com>
2018-01-09 00:36:19 +0530

    Added Travis conf 2

---
+++ config.main.py
@@ -0,0 +1,57 @@
+import os
+
+import datetime
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = os.urandom(24)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+COOKIE_VAR = '__shortto_url__'
+JWT_HEADER_NAME = 'Auth'
+JWT_EXPIRATION_DELTA = datetime.timedelta(minutes=15)
+JWT_NOT_BEFORE_DELTA = datetime.timedelta(seconds=1)
+
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'mysql://root:mysqlmysql@shortto.co8zric1dcsi.us-east-1.rds.amazonaws.com:3306/shortto'
+BASE_URL = 'https://www.shortto.com/'
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+blacklist = [
+    'shortto.com',
+    'tinyurl.com',
+    'goo.gl',
+    'is.gd',
+    'bit.ly',
+    'bit.do',
+    't.co',
+    'lnkd.in',
+    'db.tt',
+    'qr.ae',
+    'adf.ly',
+    'cur.lv',
+    'ow.ly',
+    'ity.im',
+    'q.gs',
+    'po.st',
+    'bc.bc',
+    'twitthis.com',
+    'u.to',
+    'j.mp',
+    'buzurl.com',
+    'cutt.es',
+    'u.bb',
+    'urls.org',
+    'x.co',
+    'prettylinkpro.com',
+    'scrnch.me',
+    'filoops.info',
+    'vzturl.com'
+    'qr.net',
+    'oneurl.com',
+    'tweez.me',
+    'v.gd',
+    'tr.im',
+    'link.zip.net',
+    'tinyarrows.com'
+]
